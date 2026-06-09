0ad6de3c47cde7f436e107ac1b7fa088d0b50d27
adrmrt <adrian.e.martinez.ayuso@gmail.com>
2026-06-09 12:37:00 +0200

    chore: small changes and fixes

--- src/main/java/dev/zawarudo/holo/core/command/ExecutableCommand.java
+++ src/main/java/dev/zawarudo/holo/core/command/ExecutableCommand.java
@@ -2,7 +2,15 @@ package dev.zawarudo.holo.core.command;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Interface representing the individual commands of the bot.
+ */
 public interface ExecutableCommand {
 
+    /**
+     * Executes the command for the given invocation context.
+     *
+     * @param ctx the immutable context for this invocation.
+     */
     void execute(@NotNull CommandContext ctx);
 }
--- src/main/resources/db/migration/V2__xkcd_fts.sql
+++ src/main/resources/db/migration/V2__xkcd_fts.sql
@@ -1,44 +1,33 @@
 -- Full-text search index for XKCD comics
 CREATE VIRTUAL TABLE IF NOT EXISTS XkcdComics_fts
-    USING fts5
-(
-    title,
-    alt,
-    content='XkcdComics',
-    content_rowid='id',
-    tokenize='unicode61'
+    USING fts5(
+                  title,
+                  alt,
+                  content='XkcdComics',
+                  content_rowid='id',
+                  tokenize='unicode61'
 );
 
 -- Insert trigger
-CREATE TRIGGER IF NOT EXISTS XkcdComics_ai
-    AFTER INSERT
-    ON XkcdComics
-BEGIN
+CREATE TRIGGER IF NOT EXISTS XkcdComics_ai AFTER INSERT ON XkcdComics BEGIN
     INSERT INTO XkcdComics_fts(rowid, title, alt)
     VALUES (new.id, new.title, new.alt);
 END;
 
 -- Delete trigger
-CREATE TRIGGER IF NOT EXISTS XkcdComics_ad
-    AFTER DELETE
-    ON XkcdComics
-BEGIN
+CREATE TRIGGER IF NOT EXISTS XkcdComics_ad AFTER DELETE ON XkcdComics BEGIN
     INSERT INTO XkcdComics_fts(XkcdComics_fts, rowid, title, alt)
-    VALUES ('delete', old.id, old.title, old.alt);
+    VALUES('delete', old.id, old.title, old.alt);
 END;
 
 -- Update trigger
-CREATE TRIGGER IF NOT EXISTS XkcdComics_au
-    AFTER UPDATE
-    ON XkcdComics
-BEGIN
+CREATE TRIGGER IF NOT EXISTS XkcdComics_au AFTER UPDATE ON XkcdComics BEGIN
     INSERT INTO XkcdComics_fts(XkcdComics_fts, rowid, title, alt)
-    VALUES ('delete', old.id, old.title, old.alt);
+    VALUES('delete', old.id, old.title, old.alt);
 
     INSERT INTO XkcdComics_fts(rowid, title, alt)
     VALUES (new.id, new.title, new.alt);
 END;
 
 -- Build index for existing rows
-INSERT INTO XkcdComics_fts(XkcdComics_fts)
-VALUES ('rebuild');
+INSERT INTO XkcdComics_fts(XkcdComics_fts) VALUES('rebuild');
